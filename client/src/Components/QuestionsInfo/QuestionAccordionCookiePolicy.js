6a2b3af15fb99527089929c6ff86437e8d5ec746
Milan Petrovic <milan.petrovic@vegait.rs>
2022-03-04 13:00:32 +0100

    Add link class

--- client/src/Components/QuestionsInfo/QuestionAccordionCookiePolicy.js
+++ client/src/Components/QuestionsInfo/QuestionAccordionCookiePolicy.js
@@ -7,7 +7,7 @@ class QuestionAccordionCookiePolicy extends Component {
             <button className="accordion__btn js-open-accordion">
                 <span className="font-ico-chevron-down"></span> Koliko je sigurna moja privatnost? </button>
             <div className="accordion__content">
-                <p>Podaci koje ste uneli u sistem neće biti javno dostupni i služe samo za realizovanje ideje ove aplikacije. Sa obradom podataka se možete detaljnije upoznati kroz <a href="/privacy">Politiku privatnosti.</a> </p>
+                <p>Podaci koje ste uneli u sistem neće biti javno dostupni i služe samo za realizovanje ideje ove aplikacije. Sa obradom podataka se možete detaljnije upoznati kroz <a class="footer__link" href="/privacy">Politiku privatnosti.</a> </p>
             </div>
         </div>
         );
